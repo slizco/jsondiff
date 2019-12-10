ae87934e30f330060fddc4cbaf568097d3c30b62
slizco <teamslizco@gmail.com>
2019-12-10 15:27:24 -0500

    Refactor

--- jsondiff.go
+++ jsondiff.go
@@ -3,8 +3,6 @@ package jsondiff
 import (
 	"bytes"
 	"encoding/json"
-	"reflect"
-	"strconv"
 )
 
 type Difference int
@@ -16,6 +14,10 @@ const (
 	FirstArgIsInvalidJson
 	SecondArgIsInvalidJson
 	BothArgsAreInvalidJson
+
+	// Supported output languages
+	JSON = "JSON"
+	YAML = "YAML"
 )
 
 func (d Difference) String() string {
@@ -60,6 +62,7 @@ func DefaultConsoleOptions() Options {
 		Removed: Tag{Begin: "\033[0;31m", End: "\033[0m"},
 		Changed: Tag{Begin: "\033[0;33m", End: "\033[0m"},
 		Indent:  "    ",
+		Output:  JSON,
 	}
 }
 
@@ -71,192 +74,15 @@ func DefaultHTMLOptions() Options {
 		Removed: Tag{Begin: `<span style="background-color: #fd7f7f">`, End: `</span>`},
 		Changed: Tag{Begin: `<span style="background-color: #fcff7f">`, End: `</span>`},
 		Indent:  "    ",
+		Output:  JSON,
 	}
 }
 
 // WithYAMLOutput modifies the given options for writing YAML output
 func (opts Options) WithYAMLOutput() Options {
-	opts.Indent = "  "   // indent by only two spaces in YAML
-	opts.Output = "YAML" // set language to YAML
+	opts.Indent = "  " // indent by only two spaces in YAML
+	opts.Output = YAML // set language to YAML
 	return opts
-}
-
-type context struct {
-	opts    *Options
-	buf     bytes.Buffer
-	level   int
-	lastTag *Tag
-	diff    Difference
-}
-
-func (ctx *context) Y() bool {
-	return ctx.opts.Output == "YAML"
-}
-
-func (ctx *context) newline(s string) {
-	ctx.buf.WriteString(s)
-	if ctx.lastTag != nil {
-		ctx.buf.WriteString(ctx.lastTag.End)
-	}
-	ctx.buf.WriteString("\n")
-	ctx.buf.WriteString(ctx.opts.Prefix)
-	for i := 0; i < ctx.level; i++ {
-		ctx.buf.WriteString(ctx.opts.Indent)
-	}
-	if ctx.lastTag != nil {
-		ctx.buf.WriteString(ctx.lastTag.Begin)
-	}
-}
-
-func (ctx *context) key(k string) {
-	if !ctx.Y() {
-		// only need quotes if JSON
-		k = strconv.Quote(k)
-	}
-	ctx.buf.WriteString(k)
-	ctx.buf.WriteString(": ")
-}
-
-func (ctx *context) writeValue(v interface{}, full bool) {
-	switch vv := v.(type) {
-	case bool:
-		ctx.buf.WriteString(strconv.FormatBool(vv))
-	case json.Number:
-		ctx.buf.WriteString(string(vv))
-	case string:
-		if !ctx.Y() {
-			// only need quotes if JSON
-			vv = strconv.Quote(vv)
-		}
-		ctx.buf.WriteString(vv)
-	case []interface{}:
-		if full {
-			ctx.writeList(vv)
-		} else {
-			ctx.buf.WriteString("[]")
-		}
-	case map[string]interface{}:
-		if full {
-			ctx.writeMap(vv)
-		} else {
-			ctx.buf.WriteString("{}")
-		}
-	default:
-		ctx.buf.WriteString("null")
-	}
-
-	ctx.writeTypeMaybe(v)
-}
-
-func (ctx *context) writeTypeMaybe(v interface{}) {
-	if ctx.opts.PrintTypes {
-		ctx.buf.WriteString(" ")
-		ctx.writeType(v)
-	}
-}
-
-func (ctx *context) writeType(v interface{}) {
-	switch v.(type) {
-	case bool:
-		ctx.buf.WriteString("(boolean)")
-	case json.Number:
-		ctx.buf.WriteString("(number)")
-	case string:
-		ctx.buf.WriteString("(string)")
-	case []interface{}:
-		ctx.buf.WriteString("(array)")
-	case map[string]interface{}:
-		ctx.buf.WriteString("(object)")
-	default:
-		ctx.buf.WriteString("(null)")
-	}
-}
-
-func (ctx *context) writeMismatch(a, b interface{}) {
-	ctx.writeValue(a, false)
-	ctx.buf.WriteString(" => ")
-	ctx.writeValue(b, false)
-}
-
-func (ctx *context) tag(tag *Tag) {
-	if ctx.lastTag == tag {
-		return
-	} else if ctx.lastTag != nil {
-		ctx.buf.WriteString(ctx.lastTag.End)
-	}
-	ctx.buf.WriteString(tag.Begin)
-	ctx.lastTag = tag
-}
-
-func (ctx *context) result(d Difference) {
-	if d == NoMatch {
-		ctx.diff = NoMatch
-	} else if d == SupersetMatch && ctx.diff != NoMatch {
-		ctx.diff = SupersetMatch
-	} else if ctx.diff != NoMatch && ctx.diff != SupersetMatch {
-		ctx.diff = FullMatch
-	}
-}
-
-func (ctx *context) printMismatch(a, b interface{}) {
-	ctx.tag(&ctx.opts.Changed)
-	ctx.writeMismatch(a, b)
-}
-
-func (ctx *context) printDiff(a, b interface{}) {
-	if a == nil || b == nil {
-		if a == nil && b == nil {
-			ctx.tag(&ctx.opts.Normal)
-			ctx.writeValue(a, false)
-			ctx.result(FullMatch)
-		} else {
-			ctx.printMismatch(a, b)
-			ctx.result(NoMatch)
-		}
-		return
-	}
-
-	ka := reflect.TypeOf(a).Kind()
-	kb := reflect.TypeOf(b).Kind()
-	if ka != kb {
-		ctx.printMismatch(a, b)
-		ctx.result(NoMatch)
-		return
-	}
-	switch ka {
-	case reflect.Bool:
-		if a.(bool) != b.(bool) {
-			ctx.printMismatch(a, b)
-			ctx.result(NoMatch)
-			return
-		}
-	case reflect.String:
-		switch aa := a.(type) {
-		case json.Number:
-			bb, ok := b.(json.Number)
-			if !ok || aa != bb {
-				ctx.printMismatch(a, b)
-				ctx.result(NoMatch)
-				return
-			}
-		case string:
-			bb, ok := b.(string)
-			if !ok || aa != bb {
-				ctx.printMismatch(a, b)
-				ctx.result(NoMatch)
-				return
-			}
-		}
-	case reflect.Slice:
-		ctx.printSliceDiff(a, b)
-		return
-	case reflect.Map:
-		ctx.printMapDiff(a, b)
-		return
-	}
-	ctx.tag(&ctx.opts.Normal)
-	ctx.writeValue(a, true)
-	ctx.result(FullMatch)
 }
 
 // Compares two JSON documents using given options. Returns difference type and
@@ -300,6 +126,11 @@ func Compare(a, b []byte, opts *Options) (Difference, string) {
 	}
 	if errB != nil {
 		return SecondArgIsInvalidJson, "second argument is invalid json"
+	}
+
+	if opts.Output == "" {
+		// Default to JSON if not set
+		opts.Output = JSON
 	}
 
 	ctx := context{opts: opts}
